d71d9865effb9fba038db0d03e4e1dd12b994034
magdalenahuget <magdalena.huget@gmail.com>
2023-12-15 22:54:47 +0100

    Initial commit

---
+++ src/main/java/com/example/datastructurestypes/DataStructuresTypesApplication.java
@@ -0,0 +1,13 @@
+package com.example.datastructurestypes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructuresTypesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructuresTypesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datastructurestypes/DataStructuresTypesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datastructurestypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresTypesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
