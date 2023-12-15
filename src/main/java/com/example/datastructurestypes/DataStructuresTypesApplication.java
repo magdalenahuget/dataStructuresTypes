523cac17bf03712362e837631351bc1e1e196451
magdalenahuget <magdalena.huget@gmail.com>
2023-12-15 22:55:49 +0100

    Add spring maven template files

--- src/main/java/com/example/datastructurestypes/DataStructuresTypesApplication.java
+++ src/main/java/com/example/datastructurestypes/DataStructuresTypesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DataStructuresTypesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataStructuresTypesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructuresTypesApplication.class, args);
+    }
 
 }
