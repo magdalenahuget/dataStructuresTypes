e70a4b80da4de7dde93196b0b38c722f2f4357ff
magdalenahuget <magdalena.huget@gmail.com>
2023-12-17 23:07:04 +0100

    Add empty lists generators

--- src/main/java/com/example/datastructurestypes/listInteface/LinkedListService.java
+++ src/main/java/com/example/datastructurestypes/listInteface/LinkedListService.java
@@ -1,4 +1,11 @@
 package com.example.datastructurestypes.listInteface;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class LinkedListService {
+    public List<String> getEmptyLinkedList() {
+        List<String> linkedList = new LinkedList<>();
+        return linkedList;
+    }
 }
--- src/main/java/com/example/datastructurestypes/listInteface/StackService.java
+++ src/main/java/com/example/datastructurestypes/listInteface/StackService.java
@@ -1,4 +1,11 @@
 package com.example.datastructurestypes.listInteface;
 
+import java.util.List;
+import java.util.Stack;
+
 public class StackService {
+    public List<String> getEmptyStack() {
+        List<String> stack = new Stack<>();
+        return stack;
+    }
 }
