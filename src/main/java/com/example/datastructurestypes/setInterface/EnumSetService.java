ff123438e7e5d8efb6f12f8c8aa4494bd6e74eb0
magdalenahuget <magdalena.huget@gmail.com>
2023-12-17 23:06:48 +0100

    Add other sets empty collections generators

--- src/main/java/com/example/datastructurestypes/setInterface/EnumSetService.java
+++ src/main/java/com/example/datastructurestypes/setInterface/EnumSetService.java
@@ -1,4 +1,15 @@
 package com.example.datastructurestypes.setInterface;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public class EnumSetService {
+
+    public Set<Day> getEmptyEnumSet() {
+        Set<Day> enumSet = EnumSet.of(Day.MONDAY, Day.WEDNESDAY, Day.FRIDAY);
+        return enumSet;
+    }
 }
+
+enum Day { SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY }
+
--- src/main/java/com/example/datastructurestypes/setInterface/HashSetServie.java
+++ src/main/java/com/example/datastructurestypes/setInterface/HashSetServie.java
@@ -1,4 +1,12 @@
 package com.example.datastructurestypes.setInterface;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class HashSetServie {
+
+    public Set<String> getEmptyEnumSet() {
+        Set<String> treeSet = new TreeSet<>();
+        return treeSet;
+    }
 }
