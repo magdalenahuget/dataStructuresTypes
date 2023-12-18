43aa741d6130d00e1e9c54752778ebb7201de427
magdalenahuget <magdalena.huget@gmail.com>
2023-12-18 23:39:55 +0100

    Add empty queue collection

--- src/main/java/com/example/datastructurestypes/queueInterface/QueueService.java
+++ src/main/java/com/example/datastructurestypes/queueInterface/QueueService.java
@@ -1,4 +1,12 @@
 package com.example.datastructurestypes.queueInterface;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueService {
+
+    public Queue<String> getEmptyQueue() {
+        Queue<String> queue = new LinkedList<>();
+        return queue;
+    }
 }
