4d9e4ae93a068b12c347ede4d499e1685842bf37
magdalenahuget <magdalena.huget@gmail.com>
2023-12-18 23:40:22 +0100

    Fix typo

--- src/main/java/com/example/datastructurestypes/dequeInterface/DequeService.java
+++ src/main/java/com/example/datastructurestypes/dequeInterface/DequeService.java
@@ -3,7 +3,7 @@ package com.example.datastructurestypes.dequeInterface;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-public class DequeueService {
+public class DequeService {
 
     public Deque<String> getEmptyDeque() {
         Deque<String> deque = new ArrayDeque<>();
